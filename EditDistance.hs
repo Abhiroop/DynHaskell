50795a84b1f00419e469ea5b4e64371f4ad1b617
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-11-01 13:13:28 +0000

    The improved memoized function

--- EditDistance.hs
+++ EditDistance.hs
@@ -1,5 +1,9 @@
 module EditDistance where
 
+import Data.Array as Array
+import Data.List(minimumBy)
+import Data.Ord(comparing)
+
 naive a b = d (length a) (length b)
   where d i 0 = i
         d 0 j = j
@@ -8,4 +12,67 @@ naive a b = d (length a) (length b)
           | otherwise = minimum [ d (i - 1) j         + 1
                                 , d i      (j -1)     + 1
                                 , d (i -1) (j - 1)    + 1]
+basic a b = d m n
+  where (m, n) = (length a, length b)
+        d i 0 = i
+        d 0 j = j
+        d i j
+          | a !! (i - 1) ==  b !! (j - 1) = ds ! (i - 1, j - 1)
+          | otherwise = minimum [ ds ! (i - 1, j)     + 1
+                                , ds ! (i, j - 1)     + 1
+                                , ds ! (i - 1, j - 1) + 1
+                                ]
 
+        ds = Array.listArray bounds
+               [d i j | (i, j) <- Array.range bounds]
+
+        bounds = ((0, 0), (m, n))
+
+better a b = d m n
+  where (m,n) = (length a, length b)
+        a'    = Array.listArray (1,m) a
+        b'    = Array.listArray (1,n) b
+
+        d i 0 = i
+        d 0 j = j
+        d i j
+          | a' ! i == b' ! j = ds ! (i - 1, j - 1)
+          | otherwise = minimum [ ds ! (i - 1, j)     + 1
+                                , ds ! (i, j - 1)     + 1
+                                , ds ! (i - 1, j - 1) + 1
+                                ]
+
+        ds = Array.listArray bounds
+               [d i j | (i,j) <- Array.range bounds]
+
+        bounds = ((0,0),(m,n))
+
+data Action = None | Add | Remove | Modify
+type Distance = Integer
+
+script :: Eq a => (Action -> Distance) -> [a] -> [a] -> [Action]
+script cost a b = reverse . snd $ d m n
+  where (m, n) = (length a, length b)
+        a'     = Array.listArray (1, m) a
+        b'     = Array.listArray (1, n) b
+
+        d 0 0 = (0, [])
+        d i 0 = go (i - 1) 0 Remove
+        d 0 j = go 0 (j - 1) Add
+        d i j
+          | a' ! i ==  b' ! j = go (i - 1) (j - 1) None
+          | otherwise = minimum' [ go (i - 1) j       Remove
+                                 , go i (j - 1)       Add
+                                 , go (i - 1) (j - 1) Modify
+                                 ]
+
+        minimum' = minimumBy (comparing fst)
+        go i j action = let (score, actions) = ds ! (i, j) in
+          (score + cost action, action : actions)
+
+        ds = Array.listArray bounds [d i j | (i, j) <- Array.range bounds]
+        bounds = ((0, 0), (m, n))
+
+cost :: Action -> Distance
+cost None = 0
+cost _    = 1
--- Fibo.hs
+++ Fibo.hs
@@ -1,7 +1,7 @@
 module Fibo where
 
 -- Because DP is incomplete without an example of Fibonacci
-import Data.Array
+import Data.Array as Array
 
 fib n = fibs !! n
   where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
@@ -10,5 +10,5 @@ fib' max = go max
   where go 0 = 0
         go 1 = 1
         go n = fibs ! (n - 1) + fibs ! (n - 2)
-        fibs = listArray (0, max) [go x | x <- [0..max]]
+        fibs = Array.listArray (0, max) [go x | x <- [0..max]]
 
