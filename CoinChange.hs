bbf692c594ba9fbfe5d2f58b7129af82181045e1
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-11-03 00:11:46 +0000

    Basic types and derive functor

--- CoinChange.hs
+++ CoinChange.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveFunctor #-}
 module CoinChange where
 
 import Control.Arrow ((>>>))
@@ -14,3 +15,15 @@ histo :: Functor f => CVAlgebra f a -> Term f -> a
 histo h = out >>> fmap worker >>> h where
   worker t = Attr (histo h t) (fmap worker (out t))
 
+type Cent = Int
+
+coins :: [Cent]
+coins = [50, 25, 10, 5, 1]
+
+data Nat a 
+    = Zero
+    | Next a
+      deriving Functor
+
+change :: Cent -> Int
+change = undefined
