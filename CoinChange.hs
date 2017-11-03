59ba5e22fa3a0592cefbb48ea0bef49e3e5d643d
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-11-03 00:07:25 +0000

    defining the histomorphism

--- CoinChange.hs
+++ CoinChange.hs
@@ -1,1 +1,16 @@
 module CoinChange where
+
+import Control.Arrow ((>>>))
+
+newtype Term f = In {out :: f (Term f)}
+
+data Attr f a = Attr
+              { attribute :: a
+              , hole      :: f (Attr f a)}
+
+type CVAlgebra f a = f (Attr f a) -> a
+
+histo :: Functor f => CVAlgebra f a -> Term f -> a
+histo h = out >>> fmap worker >>> h where
+  worker t = Attr (histo h t) (fmap worker (out t))
+
