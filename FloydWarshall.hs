5a078e2cbe92044866a1f067c65fb7f99f1e69fd
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-11-06 16:28:52 +0000

    A simpler Floyd Warshall skeleton

--- FloydWarshall.hs
+++ FloydWarshall.hs
@@ -1,42 +1,20 @@
 module FloydWarshall where
 
-import Data.IntMap.Lazy
-import Data.Array
-import Prelude hiding (lookup)
-import Data.Maybe
--- data IntMap Array
--- naiveFW n = d n n n
---   where d i j 0 =
+import Data.IntMap.Lazy as Map hiding (foldl')
+import Data.List
 
-inf = maxBound :: Int
+type Vertex = Int
+type Weight = Int
 
-{-
-n1 [(n1,0) (n2,8) (n3,2) (n4,inf)]
-n2 [(n2,8) (n2,0) (n3,inf) (n4,1)]
-n3 [(n1,2) (n2,inf)(n3,0) (n4,3)]
-n4 [(n1,inf) (n2,1) (n3,3) (n4,0)]
+type Graph = IntMap (IntMap Weight)
 
-d i j = min ((d i j k) , (d i k+1 k) + (d k+1 j k))
--}
+weight :: Graph -> Vertex -> Vertex -> Maybe Weight
+weight g i j = do
+  jmap <- Map.lookup i g
+  Map.lookup j jmap
 
-type X = Int
-type Y = Int
-
-type Graph = IntMap (Array Y Int)
-
-graph :: Graph
-graph = let
-  c1 = listArray (1,4) [0,8,2,inf]
-  c2 = listArray (1,4) [8,0,inf,1]
-  c3 = listArray (1,4) [2,inf,0,3]
-  c4 = listArray (1,4) [inf,1,3,0]
-  in fromList [(1,c1),(2,c2),(3,c3),(4,c4)]
-
-(!>) :: Graph -> X -> Y -> Int
-(!>) g x y = ((g Data.IntMap.Lazy.! x) Data.Array.! y)
-
-shortestPaths :: Graph -> Graph
-shortestPaths g = g--d i j k
-  where (i,j,k) = (size g, size g, size g)
-        d i j 0 = graph !> i j
-        d i j k = undefined
+shortestPaths :: [Vertex] -> Graph -> Graph
+shortestPaths vs g = foldl' update g vs
+  where
+    update :: Graph -> Vertex -> Graph
+    update = undefined
